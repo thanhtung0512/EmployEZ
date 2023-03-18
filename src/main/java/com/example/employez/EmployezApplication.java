cde515a243e410eaf36e36c7e980e9b5fa211b65
Thanh Tung <21020399@vnu,edu.vn>
2023-03-18 18:03:48 +0700

    first commit

---
+++ src/main/java/com/example/employez/EmployezApplication.java
@@ -0,0 +1,21 @@
+package com.example.employez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class EmployezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployezApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String homepage() {
+        return "homepage";
+    }
+}
---
+++ src/test/java/com/example/employez/EmployezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
