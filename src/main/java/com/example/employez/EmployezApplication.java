75a0a2cf1df43d59b803e1632b4f81069d358cf8
Thanh Tung <21020399@vnu,edu.vn>
2023-04-03 00:47:28 +0700

    config modelMapper to transfer from Entity class -> DTO class

--- src/main/java/com/example/employez/EmployezApplication.java
+++ src/main/java/com/example/employez/EmployezApplication.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 @EnableJpaRepositories(value = "com.example.employez.repository")
 public class EmployezApplication {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(EmployezApplication.class, args);
     }
